568b79e1c9c59b420b59ff870f1d06497e3bcfef
etsuko84 <etsukosone@gmail.com>
2018-10-02 22:20:04 +0900

    Incremental serach

---
+++ app/assets/javascripts/channels/index.js
@@ -0,0 +1,67 @@
+$(function() {
+var search_list = $("#user-search-result");
+
+function appendUser(user) {
+  var html = `<div class="chat-group-user clearfix">
+  <p class="chat-group-user__name">${ user.name }</p>
+  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</a>
+</div>`
+search_list.append(html);
+}
+
+function addMember(user) {
+
+  var addUser = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+  <input name='group[user_ids][]' type='hidden' value="${ user.userId }">
+  <p class='chat-group-user__name'>${ user.userName }</p>
+  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+</div>`
+$("#chat-group-user-8").after(addUser);
+}
+
+function removeUser(user) {
+var name = user.userName
+search_list.children().remove(":contains('"+name+"')");
+}
+
+function removeMenber(user) {
+$(".chat-group-users.js-add-user").children().remove(":contains('"+user+"')");
+console.log(":contains('"+user+"')")
+}
+
+  $("#user-search-field").on("keyup", function() {
+    var input = $("#user-search-field").val();
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+
+    .done(function(users){
+      $("#user-search-result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user){
+          appendUser(user);
+        });
+      }
+    })
+    .fail(function(){
+      alert('ユーザー検索に失敗しました');
+    })
+  });
+        $(document).on("click", ".user-search-add.chat-group-user__btn.chat-group-user__btn--add", function(){
+        var thisUser = $(this).data();
+          addMember(thisUser);
+          removeUser(thisUser);
+        });
+
+        $(document).on("click", ".user-search-remove.chat-group-user__btn.chat-group-user__btn--remove.js-remove-btn", function(){
+        var thisUser = $(this).siblings().text();
+          removeMenber(thisUser);
+          console.log($(this).siblings())
+      });
+});
+
+
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,14 +12,10 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to root_path, notice: 'グループを作成しました'
+      redirect_to root_path(@group), notice: 'グループを作成しました'
     else
       render :new
     end
-  end
-
-  def group_params
-    params.require(:group).permit(:name, { :user_ids =>[]})
   end
 
   def update
